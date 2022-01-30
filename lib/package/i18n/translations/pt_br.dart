08811fa77950038c07cf77128a914742233c2539
Nicollas1705 <nicollasmota1705@gmail.com>
2022-01-30 05:20:35 -0300

    Added basic translation for pt_br

---
+++ lib/package/i18n/translations/pt_br.dart
@@ -0,0 +1,144 @@
+import 'package:flutter_easy_tools/package/helpers/language/all_exports.dart';
+
+class PtBrBasicTranslator implements IBasicTranslator {
+  @override
+  String get and => "e";
+
+  @override
+  String get april => "abril";
+
+  @override
+  String get august => "agosto";
+
+  @override
+  String get centuries => "séculos";
+
+  @override
+  String get century => "século";
+
+  @override
+  String get day => "dia";
+
+  @override
+  String get days => "dias";
+
+  @override
+  String get decade => "década";
+
+  @override
+  String get decades => "décadas";
+
+  @override
+  String get december => "dezembro";
+
+  @override
+  String get february => "fevereiro";
+
+  @override
+  String get friday => "sexta";
+
+  @override
+  String get from => "de";
+
+  @override
+  String get hour => "hora";
+
+  @override
+  String get hours => "horas";
+
+  @override
+  String get january => "janeiro";
+
+  @override
+  String get july => "julho";
+
+  @override
+  String get june => "junho";
+
+  @override
+  String get march => "março";
+
+  @override
+  String get may => "maio";
+
+  @override
+  String get millennia => "milênios";
+
+  @override
+  String get millennium => "milênio";
+
+  @override
+  String get millisecond => "milissegundo";
+
+  @override
+  String get milliseconds => "milissegundos";
+
+  @override
+  String get minute => "minuto";
+
+  @override
+  String get minutes => "minutos";
+
+  @override
+  String get monday => "segunda";
+
+  @override
+  String get month => "mês";
+
+  @override
+  String get months => "meses";
+
+  @override
+  String get november => "novembro";
+
+  @override
+  String get october => "outubro";
+
+  @override
+  String get saturday => "sábado";
+
+  @override
+  String get second => "segundo";
+
+  @override
+  String get seconds => "segundos";
+
+  @override
+  String get september => "setembro";
+
+  @override
+  String get sunday => "domingo";
+
+  @override
+  String get thursday => "quinta";
+
+  @override
+  String get today => "hoje";
+
+  @override
+  String get tomorrow => "amanhã";
+
+  @override
+  String get tuesday => "terça";
+
+  @override
+  String get until => "até";
+
+  @override
+  String get wednesday => "quarta";
+
+  @override
+  String get week => "semana";
+
+  @override
+  String get weeks => "semanas";
+
+  @override
+  String get year => "ano";
+
+  @override
+  String get years => "anos";
+
+  @override
+  String get yesterday => "ontem";
+}
